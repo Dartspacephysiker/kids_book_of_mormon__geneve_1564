88de7f7fc40573ed004f4bf168248741b310cc9d
S. M. Hatch <spencermarkhatch@gmail.com>
2020-04-27 06:20:45 +0200

    Fewf, up to Mosiah 11:19

--- selectionmaker.sh
+++ selectionmaker.sh
@@ -2,8 +2,15 @@ startpage=25;
 stoppage=37;
 pdfpref=OUT/ICBoM__17__April_27-May_3__Mosiah_7-10;
 licensefile=cclicense.pdf
+bomfile=kids_bom.pdf
 
-pdfseparate -f ${startpage} -l ${stoppage} kids_bom.pdf ${pdfpref}__%03d.pdf;
+echo "Chunking out pages ${startpage} to ${stoppage} ..."
+pdfseparate -f ${startpage} -l ${stoppage} ${bomfile} ${pdfpref}__%03d.pdf;
+
+echo "Making ${pdfpref}.pdf ..."
 pdfunite ${pdfpref}__*.pdf ${licensefile} ${pdfpref}.pdf
+
+echo "Removing temporary files ..."
+rm ${pdfpref}__*.pdf
 
 echo DONE
