4e689dc9ec9ebdad91735f16680e3eeb6011dc44
S. M. Hatch <spencermarkhatch@gmail.com>
2020-05-03 09:45:15 +0200

    Pacing sheet for Week 19

--- selectionmaker.sh
+++ selectionmaker.sh
@@ -1,6 +1,11 @@
-startpage=25;
-stoppage=37;
-pdfpref=OUT/ICBoM__17__April_27-May_3__Mosiah_7-10;
+startpage=37;
+stoppage=56;
+pdfpref=OUT/ICBoM__19__May_4-10__Mosiah_11-17;
+
+# startpage=25;
+# stoppage=37;
+# pdfpref=OUT/ICBoM__18__April_27-May_3__Mosiah_7-10;
+
 licensefile=cclicense.pdf
 bomfile=kids_bom.pdf
 
