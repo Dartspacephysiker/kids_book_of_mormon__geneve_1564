61e4a9890ecea950a91927244a1ffaffd10b8d2b
S. M. Hatch <spencermarkhatch@gmail.com>
2020-06-01 06:39:13 +0200

    Småting, på tide å begynne med Uke 23

--- selectionmaker.sh
+++ selectionmaker.sh
@@ -1,24 +1,41 @@
+weekno=23
+
 # pdfpref=OUT/ICBoM__20__May_18-24__Mosiah_25-28;
 
-startpage=91;
-stoppage=112;
-pdfpref=OUT/ICBoM__22__May_25-31__Mosiah_29-Alma_4;
-
-# startpage=78;
-# stoppage=91;
-# pdfpref=OUT/ICBoM__21__May_18-24__Mosiah_25-28;
-
-# startpage=56;
-# stoppage=77;
-# pdfpref=OUT/ICBoM__20__May_11-17__Mosiah_18-24;
-
-# startpage=37;
-# stoppage=56;
-# pdfpref=OUT/ICBoM__19__May_4-10__Mosiah_11-17;
-
-# startpage=25;
-# stoppage=37;
-# pdfpref=OUT/ICBoM__18__April_27-May_3__Mosiah_7-10;
+if [[ ${weekno} -eq 23 ]]
+then
+    startpage=112;
+    stoppage=126;
+    pdfpref=OUT/ICBoM__23__June_1-7__Alma_5-7;
+elif [[ ${weekno} -eq 22 ]]
+then
+    startpage=91;
+    stoppage=112;
+    pdfpref=OUT/ICBoM__22__May_25-31__Mosiah_29-Alma_4;
+elif [[ ${weekno} -eq 21 ]]
+then
+     startpage=78;
+     stoppage=91;
+     pdfpref=OUT/ICBoM__21__May_18-24__Mosiah_25-28;
+elif [[ ${weekno} -eq 20 ]]
+then
+    startpage=56;
+    stoppage=77;
+    pdfpref=OUT/ICBoM__20__May_11-17__Mosiah_18-24;
+elif [[ ${weekno} -eq 19 ]]
+then
+    startpage=37;
+    stoppage=56;
+    pdfpref=OUT/ICBoM__19__May_4-10__Mosiah_11-17;
+elif [[ ${weekno} -eq 18 ]]
+then
+    startpage=25;
+    stoppage=37;
+    pdfpref=OUT/ICBoM__18__April_27-May_3__Mosiah_7-10;
+else
+    echo "No week selected!"
+    exit
+fi
 
 licensefile=cclicense.pdf
 bomfile=kids_bom.pdf
